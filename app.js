ad0b53aa1f1a77908f837d6f840af0db6aff8b74
cfalcant <cfalcant@gmail.com>
2017-11-28 16:10:54 -0700

    updated

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ console.log("Javascript is alive!");
 function newGreeting (string) {
   document.getElementById("greeting").innerHTML = "Hello, World!";
 } newGreeting ()
+
+// function newColor () {
+//     var back = document.getElementsByTagName("li")
+
+function newColor () {
+  let targets = document.querySelectorAll("li");
+   for (let i = 0; i < targets.length; i++) {
+     targets[i].style.backgroundColor = "yellow";
+   }
+ }
+newColor ()
