654e1a101bd92ca3e23560b52eae6fdffbff6e5b
cfalcant <cfalcant@gmail.com>
2017-11-28 17:27:09 -0700

    updated

--- app.js
+++ app.js
@@ -14,3 +14,8 @@ function newColor () {
    }
  }
 newColor ()
+
+
+  let image = document.createElement("img");
+  image.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+  document.getElementById("greeting").appendChild(image);
